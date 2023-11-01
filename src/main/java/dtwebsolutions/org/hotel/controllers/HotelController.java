b002473b516f1745c3939b56332c49217f3eb770
Jagdesh Kumar <wkis175@live.rhul.ac.uk>
2023-11-02 02:14:50 +0500

    Refactor the code and implemented the exception handling

---
+++ src/main/java/dtwebsolutions/org/hotel/controllers/HotelController.java
@@ -0,0 +1,44 @@
+package dtwebsolutions.org.hotel.controllers;
+
+import dtwebsolutions.org.hotel.entites.Hotel;
+import dtwebsolutions.org.hotel.services.HotelService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/hotels")
+public class HotelController {
+
+    private final HotelService hotelService;
+
+    public HotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
+    //create
+
+    @PostMapping
+    public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(hotelService.create(hotel));
+    }
+
+
+    //get single
+
+    @GetMapping("/{hotelId}")
+    public ResponseEntity<Hotel> createHotel(@PathVariable String hotelId) {
+        return ResponseEntity.status(HttpStatus.OK).body(hotelService.get(hotelId));
+    }
+
+
+    //get all
+    @GetMapping
+    public ResponseEntity<List<Hotel>> getAll() {
+        return ResponseEntity.ok(hotelService.getAll());
+    }
+
+
+}
